2b44b434356ab3ff8384455628143b45dcec92d3
haider2017 <i120223@nu.edu.pk>
2019-09-06 15:22:26 +0500

    deleting older version of backend microservice

--- seneca_routes/index.js
+++
@@ -1,9 +0,0 @@
-var seneca = require('seneca')();
-
-seneca.add('timeout$: 300000,role:client,cmd:salestax', function (msg, done) {
-    var rate  = 0.23
-    var total = msg.net * (1 + rate)
-    done(null, {total: total})
-}).listen()
-
-module.exports = seneca;
