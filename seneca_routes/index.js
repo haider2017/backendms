86c7fcbd232eb14008a63be811609c949da41b67
haider2017 <i120223@nu.edu.pk>
2019-09-06 12:40:48 +0500

    added timeout

--- seneca_routes/index.js
+++ seneca_routes/index.js
@@ -1,6 +1,6 @@
 var seneca = require('seneca')();
 
-seneca.add('role:client,cmd:salestax', function (msg, done) {
+seneca.add('timeout$: 300000,role:client,cmd:salestax', function (msg, done) {
     var rate  = 0.23
     var total = msg.net * (1 + rate)
     done(null, {total: total})
